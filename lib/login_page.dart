8066748abde9af14d7d97fae4245415d8e1e6106
Stefan Vlad <72700839+StefanVlad0@users.noreply.github.com>
2023-02-23 12:53:30 +0200

    login design -m

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -18,97 +18,124 @@ class _LoginPageState extends State<LoginPage> {
           backgroundColor: const Color.fromARGB(255, 0, 0, 0),
           resizeToAvoidBottomInset: false,
           appBar: AppBar(
-            backgroundColor: Color.fromARGB(255, 0, 0, 0),
+            backgroundColor: const Color.fromARGB(255, 0, 0, 0),
             elevation: 0,
           ),
           body: SafeArea(
-            child: Stack(
-              alignment: Alignment.topCenter, // defult topLeft
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        decoration: const BoxDecoration(
-                            gradient: RadialGradient(
-                                center: Alignment.topLeft,
-                                radius: 0.8,
-                                colors: [
-                              Color.fromRGBO(51, 112, 255, 0.75),
-                              Color.fromARGB(255, 0, 0, 0),
-                            ],
-                                stops: [
-                              0.1,
-                              1,
-                            ])),
-                      ),
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color.fromRGBO(45, 58, 90, 1),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  height: 600,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 25),
+                        Text("Login",
+                            style: GoogleFonts.roboto(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              letterSpacing: 2,
+                              fontSize: 32,
+                            )),
+                        const SizedBox(height: 25),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 60),
+                          child: Text(
+                              "Hey, enter your details to login to your account",
+                              textAlign: TextAlign.center,
+                              style: GoogleFonts.outfit(
+                                color: Colors.white,
+                                fontSize: 20,
+                              )),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 20, right: 20, top: 30),
+                          child: TextField(
+                            style: const TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: const Color.fromRGBO(32, 42, 66, 1),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                borderSide: const BorderSide(
+                                    color: Color.fromARGB(255, 59, 85, 255),
+                                    width: 2.0),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                borderSide: const BorderSide(
+                                    color: Color.fromARGB(255, 0, 0, 0),
+                                    width: 2.0),
+                              ),
+                              hintText: 'Email',
+                              hintStyle: const TextStyle(
+                                color: Color.fromRGBO(255, 255, 255, 1),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 20),
+                          child: TextField(
+                            obscureText: true,
+                            style: const TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: const Color.fromRGBO(32, 42, 66, 1),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                borderSide: const BorderSide(
+                                    color: Color.fromARGB(255, 59, 85, 255),
+                                    width: 2.0),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                borderSide: const BorderSide(
+                                    color: Color.fromARGB(255, 0, 0, 0),
+                                    width: 2.0),
+                              ),
+                              hintText: 'Password',
+                              hintStyle: const TextStyle(
+                                color: Color.fromRGBO(255, 255, 255, 1),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 100,
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                const Color.fromRGBO(51, 112, 255, 0.75),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 85, vertical: 20),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50)),
+                          ),
+                          onPressed: (() => context.push('/')),
+                          child: Text('Login',
+                              style: GoogleFonts.roboto(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 26,
+                              )),
+                        ),
+                      ],
                     ),
-                    Expanded(
-                      child: Container(
-                        decoration: const BoxDecoration(
-                            gradient: RadialGradient(
-                                center: Alignment.topRight,
-                                radius: 0.8,
-                                colors: [
-                              Color.fromRGBO(51, 112, 255, 0.75),
-                              Color.fromARGB(255, 0, 0, 0),
-                            ],
-                                stops: [
-                              0.1,
-                              1,
-                            ])),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-                Column(
-                  children: [
-                    const SizedBox(
-                      height: 200,
-                    ),
-                    Image.asset(
-                      'assets/images/logo2.png',
-                      scale: 2.5,
-                    ),
-                    SizedBox(height: 25),
-                    Text("MD Planner",
-                        style: GoogleFonts.audiowide(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          letterSpacing: 2,
-                          fontSize: 32,
-                        )),
-                    const SizedBox(height: 25),
-                    Text("Stay healthy.\nStay connected...",
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.electrolize(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 20,
-                        )),
-                    const SizedBox(
-                      height: 100,
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor:
-                            const Color.fromRGBO(51, 112, 255, 0.75),
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 85, vertical: 20),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50)),
-                      ),
-                      onPressed: (() => context.push('/')),
-                      child: Text('Login',
-                          style: GoogleFonts.roboto(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                            fontSize: 26,
-                          )),
-                    ),
-                  ],
-                ),
-              ],
+              ),
             ),
           ),
         ),
@@ -152,11 +179,11 @@ class _LoginPageState extends State<LoginPage> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Text('Login'),
-                        Text(
+                        const Text('Login'),
+                        const Text(
                             'Hey, enter your details to login to your account'),
                         Form(
-                            child: Column(children: [
+                            child: Column(children: const [
                           Text('Email'),
                           Text('Password'),
                         ])),
