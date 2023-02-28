4269d88f66e357d16c8353e482b0e3a0d735374e
seerbann <serbypwp@yahoo.com>
2023-02-28 12:58:35 +0200

    Folders for list + main strucure ready

--- lib/Pages/listpage/main_listpage.dart
+++ lib/Pages/listpage/main_listpage.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:health_hub/Pages/listpage/lista_oameni.dart';
+import 'package:health_hub/Pages/listpage/components/lista_oameni.dart';
 import 'package:health_hub/responsive.dart';
+import 'components/appbar.dart';
 
 class MainListPage extends StatefulWidget {
   @override
@@ -13,40 +14,44 @@ class _MainListPageState extends State<MainListPage> {
   Widget build(BuildContext context) {
     // It provide us the width and height
     Size _size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: Responsive(
-        // Let's work on our mobile part
-        mobile: ListaOameni(),
-        tablet: Row(
-          children: [
-            Expanded(
-              flex: 6,
-              child: ListaOameni(),
+    return Responsive(
+      // Let's work on our mobile part
+      mobile: Scaffold(
+        appBar: CustomAppBar(),
+        body: ListaOameni(),
+      ),
+      tablet: Row(
+        children: [
+          Expanded(
+            flex: 6,
+            child: Scaffold(
+              appBar: CustomAppBar(),
+              body: ListaOameni(),
             ),
-            Expanded(
-              flex: 9,
-              child: Placeholder(),
-            ),
-          ],
-        ),
-        desktop: Row(
-          children: [
-            // Once our width is less then 1300 then it start showing errors
-            // Now there is no error if our width is less then 1340
-            Expanded(
-              flex: _size.width > 1340 ? 2 : 4,
-              child: Placeholder(),
-            ),
-            Expanded(
-              flex: _size.width > 1340 ? 3 : 5,
-              child: ListaOameni(),
-            ),
-            Expanded(
-              flex: _size.width > 1340 ? 8 : 10,
-              child: Placeholder(),
-            ),
-          ],
-        ),
+          ),
+          Expanded(
+            flex: 9,
+            child: Placeholder(),
+          ),
+        ],
+      ),
+      desktop: Row(
+        children: [
+          // Once our width is less then 1300 then it start showing errors
+          // Now there is no error if our width is less then 1340
+          Expanded(
+            flex: _size.width > 1340 ? 2 : 4,
+            child: Placeholder(),
+          ),
+          Expanded(
+            flex: _size.width > 1340 ? 3 : 5,
+            child: ListaOameni(),
+          ),
+          Expanded(
+            flex: _size.width > 1340 ? 8 : 10,
+            child: Placeholder(),
+          ),
+        ],
       ),
     );
   }
