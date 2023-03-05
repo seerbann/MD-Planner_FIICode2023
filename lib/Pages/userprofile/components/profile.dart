52a6bc8bbe1a982726b5b28326b42a7bcf9c52dc
seerbann <serbypwp@yahoo.com>
2023-03-05 17:14:51 +0200

    butons in navBar functionable

---
+++ lib/Pages/userprofile/components/profile.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Profile extends StatefulWidget {
+  const Profile({super.key});
+
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+        begin: Alignment.centerRight,
+        end: Alignment.centerLeft,
+        colors: [
+          Color.fromRGBO(122, 162, 255, 1),
+          Color.fromRGBO(51, 112, 255, 1),
+        ],
+      )),
+      child: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.white,
+                    child: Text('nume prenume'),
+                  ),
+                  Container(
+                    color: Colors.white,
+                    child: Text('Lorem ipsum'),
+                  )
+                ],
+              ),
+              Container(
+                color: Colors.white,
+                child: Text('Istoric Medical'),
+              )
+            ]),
+      ),
+    );
+  }
+}
