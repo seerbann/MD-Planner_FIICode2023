20017e9ec58b9bea1ba8a950073d24642ae56044
seerbann <serbypwp@yahoo.com>
2023-03-07 16:27:39 +0200

    calendar page

--- lib/Pages/calendarpage/main_calendar.dart
+++ lib/Pages/calendarpage/main_calendar.dart
@@ -4,12 +4,12 @@ import 'package:health_hub/commonComponents/navBar_desktop.dart';
 import 'package:health_hub/responsive.dart';
 import 'package:health_hub/commonComponents/appbar.dart';
 
-class MainListPage extends StatefulWidget {
+class CalendarPage extends StatefulWidget {
   @override
-  State<MainListPage> createState() => _MainListPageState();
+  State<CalendarPage> createState() => _CalendarPageState();
 }
 
-class _MainListPageState extends State<MainListPage> {
+class _CalendarPageState extends State<CalendarPage> {
   @override
   Widget build(BuildContext context) {
     // It provide us the width and height
