c1cf2665f151fc05ab6b827123a9aef7766bcaf5
Stefan Vlad <72700839+StefanVlad0@users.noreply.github.com>
2023-03-12 12:02:53 +0200

    profile data from db

--- lib/Pages/medicprofile/components/medicProfile.dart
+++ lib/Pages/medicprofile/components/medicProfile.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -12,6 +13,29 @@ class MedicProfile extends StatefulWidget {
 
   @override
   State<MedicProfile> createState() => _MedicProfileState();
+}
+
+FirebaseAuth auth = FirebaseAuth.instance;
+
+String finalString = "";
+
+Future getName() async {
+  bool procesTerminat = false;
+  await FirebaseFirestore.instance
+      .collection('users')
+      .where('email', isEqualTo: auth.currentUser?.email)
+      .get()
+      .then(
+        (snapshot) => snapshot.docs.forEach((document) {
+          print(document.reference.id);
+          Map<String, dynamic> data = document.data();
+          String firstname = data['first name'];
+          String lastname = data['last name'];
+          procesTerminat = true;
+          finalString = 'Dr. ' + firstname + ' ' + lastname;
+        }),
+      );
+  return procesTerminat;
 }
 
 CalendarFormat _calendarFormat = CalendarFormat.month;
@@ -54,13 +78,16 @@ class _MedicProfileState extends State<MedicProfile> {
                             Image.asset('assets/images/defaultUser.png'),
                             Column(
                               children: [
-                                const Text(
-                                  'Prof. Dr. Daniel Leon ',
-                                  style: TextStyle(
-                                      fontFamily: 'Roboto',
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 30,
-                                      color: Colors.black),
+                                FutureBuilder(
+                                  future: getName(),
+                                  builder: (context, snapshot) {
+                                    if (snapshot.hasData) {
+                                      return Text(finalString);
+                                    } else {
+                                      return Center(
+                                          child: CircularProgressIndicator());
+                                    }
+                                  },
                                 ),
                                 Text('no description',
                                     style: TextStyle(
