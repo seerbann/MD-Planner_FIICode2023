d7480edf451d1ab2551f94bd426d226012452e75
Stefan Vlad <72700839+StefanVlad0@users.noreply.github.com>
2023-03-13 18:50:15 +0200

    new Dynamic

--- lib/Pages/main_page.dart
+++ lib/Pages/main_page.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:health_hub/Pages/login/login_page.dart';
 import 'package:health_hub/Pages/medicprofile/main_medicprofile.dart';
 import 'package:health_hub/Pages/signin/este_medic.dart';
