fd41cb194b24c244beb4adec084e1635b8ac71e6
Stefan Vlad <72700839+StefanVlad0@users.noreply.github.com>
2023-03-19 17:09:43 +0200

    restricted pages + sort medics by fullName

--- lib/Pages/calendarpage/main_calendar.dart
+++ lib/Pages/calendarpage/main_calendar.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:health_hub/Pages/calendarpage/components/calendar.dart';
 import 'package:health_hub/commonComponents/navBar_desktop.dart';
 import 'package:health_hub/responsive.dart';
 import 'package:health_hub/commonComponents/appbar.dart';
+
+import '../login/login_page.dart';
 
 class CalendarPage extends StatefulWidget {
   @override
@@ -10,36 +13,51 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
+  bool isRestricted() {
+    bool esteRestrictionat = false;
+    if (FirebaseAuth.instance.currentUser != null) {
+      esteRestrictionat = false;
+    } else {
+      esteRestrictionat = true;
+      print('Este restrictionat');
+    }
+    return esteRestrictionat;
+  }
+
   @override
   Widget build(BuildContext context) {
     // It provide us the width and height
     Size _size = MediaQuery.of(context).size;
-    return Responsive(
-      // Let's work on our mobile part
-      mobile: const Scaffold(
-        appBar: CustomAppBar(),
-        drawer: CustomDrawer(),
-        body: Calendar(),
-      ),
-      tablet: Scaffold(
-        appBar: const CustomAppBar(),
-        drawer: CustomDrawer(),
-        body: Row(
-          children: [
-            const Expanded(flex: 6, child: Calendar()),
-          ],
+    bool esteRestrictionat = isRestricted();
+    if (esteRestrictionat) {
+      return LoginPage();
+    } else
+      return Responsive(
+        // Let's work on our mobile part
+        mobile: const Scaffold(
+          appBar: CustomAppBar(),
+          drawer: CustomDrawer(),
+          body: Calendar(),
         ),
-      ),
-      desktop: SafeArea(
-        child: Scaffold(
+        tablet: Scaffold(
+          appBar: const CustomAppBar(),
+          drawer: CustomDrawer(),
           body: Row(
-            children: const [
-              Expanded(flex: 1, child: CustomNavBar()),
-              Expanded(flex: 15, child: Calendar()),
+            children: [
+              const Expanded(flex: 6, child: Calendar()),
             ],
           ),
         ),
-      ),
-    );
+        desktop: SafeArea(
+          child: Scaffold(
+            body: Row(
+              children: const [
+                Expanded(flex: 1, child: CustomNavBar()),
+                Expanded(flex: 15, child: Calendar()),
+              ],
+            ),
+          ),
+        ),
+      );
   }
 }
--- lib/Pages/listpage/main_listpage.dart
+++ lib/Pages/listpage/main_listpage.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:go_router/go_router.dart';
 
 import 'package:health_hub/Pages/listpage/components/ListAndPacientDetails.dart';
+import 'package:health_hub/Pages/login/login_page.dart';
 import 'package:health_hub/commonComponents/navBar_desktop.dart';
 import 'package:health_hub/responsive.dart';
 import 'package:health_hub/commonComponents/appbar.dart';
@@ -13,36 +16,51 @@ class MainListPage extends StatefulWidget {
 }
 
 class _MainListPageState extends State<MainListPage> {
+  bool isRestricted() {
+    bool esteRestrictionat = false;
+    if (FirebaseAuth.instance.currentUser != null) {
+      esteRestrictionat = false;
+    } else {
+      esteRestrictionat = true;
+      print('Este restrictionat');
+    }
+    return esteRestrictionat;
+  }
+
   @override
   Widget build(BuildContext context) {
     // It provide us the width and height
     Size _size = MediaQuery.of(context).size;
     final GlobalKey<ScaffoldState> _scaffoldKey =
         new GlobalKey<ScaffoldState>();
-    return Responsive(
-      // Let's work on our mobile part
-      mobile: Scaffold(
-        appBar: CustomAppBar(),
-        drawer: CustomDrawer(),
-        body: ListAndPacientDetails(),
-      ),
-      tablet: Scaffold(
-        appBar: CustomAppBar(),
-        drawer: CustomDrawer(),
-        body: Row(
-          children: [
-            Expanded(child: ListAndPacientDetails()),
-          ],
+    bool esteRestrictionat = isRestricted();
+    if (esteRestrictionat) {
+      return LoginPage();
+    } else
+      return Responsive(
+        // Let's work on our mobile part
+        mobile: Scaffold(
+          appBar: CustomAppBar(),
+          drawer: CustomDrawer(),
+          body: ListAndPacientDetails(),
         ),
-      ),
-      desktop: SafeArea(
-        child: Row(
-          children: [
-            Expanded(flex: 1, child: CustomNavBar()),
-            Expanded(flex: 15, child: ListAndPacientDetails()),
-          ],
+        tablet: Scaffold(
+          appBar: CustomAppBar(),
+          drawer: CustomDrawer(),
+          body: Row(
+            children: [
+              Expanded(child: ListAndPacientDetails()),
+            ],
+          ),
         ),
-      ),
-    );
+        desktop: SafeArea(
+          child: Row(
+            children: [
+              Expanded(flex: 1, child: CustomNavBar()),
+              Expanded(flex: 15, child: ListAndPacientDetails()),
+            ],
+          ),
+        ),
+      );
   }
 }
